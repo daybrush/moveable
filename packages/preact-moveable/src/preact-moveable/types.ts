a585230b5d8a34d9747136a52fc180c401c1e23a
Daybrush <daybrush@gmail.com>
2019-07-13 02:33:06 +0900

    fix: fix Coponent types

--- packages/preact-moveable/src/preact-moveable/types.ts
+++ packages/preact-moveable/src/preact-moveable/types.ts
@@ -1,7 +1,7 @@
 import { MoveableProps, MoveableState } from "react-moveable/declaration/types";
-import Preact from "preact";
+import { Component } from "preact";
 
-export interface MoveableInterface extends Preact.Component<MoveableProps, MoveableState> {
+export interface MoveableInterface extends Component<MoveableProps, MoveableState> {
     isMoveableElement(target: HTMLElement | SVGElement): boolean;
     getRadByPos(pos: number[]): number;
     getDirection(): 1 | -1;
