7641b2b9cf90d6c7e9d0cf1349932085ccbd74f3
Daybrush <daybrush@gmail.com>
2019-07-29 02:31:55 +0900

    demo: Add Warpable Demo

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,19 +1,34 @@
 import Moveable from "../../src/Moveable";
 import { codes } from "./consts";
+import { Frame } from "scenejs";
 import "./index.css";
+import { hasClass } from "@daybrush/utils";
 
 declare const hljs: any;
 
 const moveableElement: HTMLElement = document.querySelector(".moveable");
 const labelElement: HTMLElement = document.querySelector(".label");
 
-let rotate: number = 0;
-const translate = [0, 0];
-const scale = [1, 1];
+const frame = new Frame({
+    width: "250px",
+    height: "200px",
+    left: "0px",
+    top: "0px",
+    transform: {
+        rotate: "0deg",
+        scaleX: 1,
+        scaleY: 1,
+        matrix3d: [
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1,
+        ],
+    },
+});
 
 function setTransform(target: HTMLElement | SVGElement) {
-    target.style.transform
-        = `translate(${translate[0]}px, ${translate[1]}px) rotate(${rotate}deg) scale(${scale[0]}, ${scale[1]})`;
+    target.style.cssText = frame.toCSS();
 }
 function setLabel(clientX: number, clientY: number, text) {
     // tslint:disable-next-line: max-line-length
@@ -31,27 +46,43 @@ const moveable = new Moveable(moveableElement.parentElement, {
     throttleDrag: 1,
     throttleScale: 0.01,
     throttleRotate: 0.2,
+    throttleResize: 1,
 }).on("drag", ({ target, left, top, clientX, clientY }) => {
-    target.style.left = `${left}px`;
-    target.style.top = `${top}px`;
+    frame.set("left", `${left}px`);
+    frame.set("top", `${top}px`);
+    setTransform(target);
     setLabel(clientX, clientY, `X: ${left}px<br/>Y: ${top}px`);
 }).on("scale", ({ target, dist, clientX, clientY }) => {
-    scale[0] *= dist[0];
-    scale[1] *= dist[1];
+    const scaleX = frame.get("transform", "scaleX") * dist[0];
+    const scaleY = frame.get("transform", "scaleY") * dist[1];
+    frame.set("transform", "scaleX", scaleX);
+    frame.set("transform", "scaleY", scaleY);
     setTransform(target);
-    setLabel(clientX, clientY, `S: ${scale[0].toFixed(2)}, ${scale[1].toFixed(2)}`);
-}).on("rotate", ({ target, beforeDelta, clientX, clientY }) => {
-    rotate += beforeDelta;
+    setLabel(clientX, clientY, `S: ${scaleX.toFixed(2)}, ${scaleY.toFixed(2)}`);
+}).on("rotate", ({ target, delta, clientX, clientY }) => {
+    const deg = parseFloat(frame.get("transform", "rotate")) + delta;
+
+    frame.set("transform", "rotate", `${deg}deg`);
     setTransform(target);
-    setLabel(clientX, clientY, `R: ${rotate.toFixed(1)}`);
-}).on("resize", ({ target, width, height }) => {
-    target.style.width = `${width}px`;
-    target.style.height = `${height}px`;
+    setLabel(clientX, clientY, `R: ${deg.toFixed(1)}`);
+}).on("resize", ({ target, width, height, clientX, clientY }) => {
+    frame.set("width", `${width}px`);
+    frame.set("height", `${height}px`);
+    setTransform(target);
+    setLabel(clientX, clientY, `W: ${width}px<br/>H: ${height}px`);
+}).on("warp", ({ target, multiply, delta, clientX, clientY }) => {
+    frame.set("transform", "matrix3d", multiply(frame.get("transform", "matrix3d"), delta));
+    setTransform(target);
+    setLabel(clientX, clientY, `X: ${clientX}px<br/>Y: ${clientY}px`);
 }).on("dragEnd", () => {
     labelElement.style.display = "none";
 }).on("scaleEnd", () => {
     labelElement.style.display = "none";
 }).on("rotateEnd", () => {
+    labelElement.style.display = "none";
+}).on("resizeEnd", () => {
+    labelElement.style.display = "none";
+}).on("warpEnd", () => {
     labelElement.style.display = "none";
 });
 
@@ -71,7 +102,7 @@ const resizable = new Moveable(resizableElement.parentElement, {
     container: resizableElement.parentElement,
     origin: false,
     resizable: true,
-}).on("resize", ({ target, width, height}) => {
+}).on("resize", ({ target, width, height }) => {
     target.style.width = `${width}px`;
     target.style.height = `${height}px`;
 });
@@ -95,6 +126,15 @@ const rotatable = new Moveable(rotatableElement.parentElement, {
 }).on("rotate", ({ target, transform }) => {
     target.style.transform = transform;
 });
+const warpableElement: HTMLElement = document.querySelector(".warpable");
+const warpable = new Moveable(warpableElement.parentElement, {
+    target: warpableElement,
+    container: warpableElement.parentElement,
+    warpable: true,
+    origin: false,
+}).on("warp", ({ target, transform }) => {
+    target.style.transform = transform;
+});
 
 const originElement: HTMLElement = document.querySelector(".origin");
 const origin = new Moveable(originElement.parentElement, {
@@ -116,6 +156,7 @@ window.addEventListener("resize", () => {
     resizable.updateRect();
     scalable.updateRect();
     rotatable.updateRect();
+    warpable.updateRect();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -132,6 +173,36 @@ document.addEventListener("DOMContentLoaded", () => {
         block.innerText = code;
         hljs.highlightBlock(block);
     });
+});
+
+const ableElement = document.querySelector(".buttons.able");
+const ableButtonElements = [].slice.call(ableElement.children);
+
+ableElement.addEventListener("click", e => {
+    const target = (e.target as HTMLElement);
+    const able = target.getAttribute("data-able");
+
+    if (!able) {
+        return;
+    }
+    ableButtonElements.forEach((el: HTMLElement) => {
+        el.classList.remove("selected");
+    });
+    target.classList.add("selected");
+
+    if (able === "warpable") {
+        moveable.resizable = false;
+        moveable.scalable = false;
+        moveable.warpable = true;
+    } else if (able === "scalable") {
+        moveable.resizable = false;
+        moveable.scalable = true;
+        moveable.warpable = false;
+    } else if (able === "resizable") {
+        moveable.resizable = true;
+        moveable.scalable = false;
+        moveable.warpable = false;
+    }
 });
 
 const tabGroups = {};
