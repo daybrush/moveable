9bad1ceb5ba4df1e668f89b3c7443f912dfa6bb1
Daybrush <daybrush@gmail.com>
2020-01-16 02:16:13 +0900

    feat: add react-compat-moveable #129

---
+++ packages/react-compat-moveable/rollup.config.js
@@ -0,0 +1,48 @@
+
+import builder from "@daybrush/builder";
+import reactCompat from "rollup-plugin-react-compat";
+
+const external = {
+    "react-simple-compat": "react-simple-compat",
+    "react-compat-ruler": "react-compat-ruler",
+    "react-compat-css-styled": "react-compat-css-styled",
+    "@daybrush/utils": "utils",
+    "css-styled": "css-styled",
+    "framework-utils": "framework-utils",
+    "@daybrush/drag": "Dragger",
+    "@egjs/agent": "eg.Agent",
+    "@egjs/children-differ": "eg.ChildrenDiffer",
+    "@moveable/matrix": "@moveable/matrix",
+};
+
+
+const reactPlugin = reactCompat({
+    useReactCompat: true,
+    aliasModules: {
+        "@scena/react-ruler": "react-compat-ruler",
+        "react-css-styled": "react-compat-css-styled",
+    }
+})
+
+
+
+export default builder([
+    {
+        sourcemap: false,
+        input: "src/index.ts",
+        output: "./dist/moveable.esm.js",
+        exports: "default",
+        format: "es",
+        plugins: [reactPlugin],
+        external,
+    },
+    {
+        sourcemap: false,
+        input: "src/index.ts",
+        output: "./dist/moveable.cjs.js",
+        exports: "default",
+        plugins: [reactPlugin],
+        format: "cjs",
+        external,
+    },
+]);
---
+++ packages/react-compat-moveable/src/index.ts
@@ -0,0 +1,3 @@
+import Moveable from "react-moveable";
+export default Moveable;
+export * from "react-moveable";
