2ec6da650d18fd079f7a7b192c4a42362a29be3b
Daybrush <daybrush@gmail.com>
2020-03-02 00:25:38 +0900

    test: fix test for rotatable

--- packages/react-moveable/test/unit/ables/RotatableExample.tsx
+++ packages/react-moveable/test/unit/ables/RotatableExample.tsx
@@ -17,7 +17,7 @@ export default class RotatableExmaple extends React.Component<{
         return (
             <div className="c1" style={{
                 position: "relative", left: "0px", top: "0px", width: "500px", height: "500px", border: "2px solid black",
-                transform: "scale(2)",
+                // transform: "scale(2)",
             }}>
                 <Moveable target={this.state.target} ref={ref(this, "innerMoveable")}
                     rotatable={true} onRotateStart={onRotateStart} onRotate={onRotate} onRotateEnd={onRotateEnd} />
