d2fec43aeed2e935a9315003f2876b7b99a46ac4
Daybrush <younkue.choi@navercorp.com>
2022-09-11 12:51:48 +0900

    fix: fix moveable container #732

--- packages/svelte-moveable/src/Moveable.svelte
+++ packages/svelte-moveable/src/Moveable.svelte
@@ -15,7 +15,8 @@
 
   const dispatch = createEventDispatcher();
   let options = {};
-  let container;
+
+  let moveableElement;
   let moveable;
 
   beforeUpdate(() => {
@@ -27,32 +28,26 @@
         options[name] = props[name];
       }
     });
-    container = options.container || $$props.container || document.body;
 
     if (moveable) {
       tick().then(() => {
         moveable.setState({
           ...options,
-          container,
-          parentElement: container
         });
       });
     }
   });
   onMount(() => {
-    moveable = new VanillaMoveable(container || document.body, options);
+    moveable = new VanillaMoveable(moveableElement, {
+      ...options
+      portalContainer: moveableElement,
+    });
 
     EVENTS.forEach(name => {
       const onName = camelize(`on ${name}`);
       moveable.on(name, e => {
-        const result = $$props[onName] && $$props[onName](e);
-        const result2 = dispatch(name, e);
-
-        return !isUndefined(result)
-          ? result
-          : !isUndefined(result2)
-          ? result2
-          : undefined;
+        $$props[onName] && $$props[onName](e);
+        dispatch(name, e);
       });
     });
   });
@@ -64,3 +59,4 @@
     return moveable;
   }
 </script>
+<div bind:this={moveableElement}></div>
