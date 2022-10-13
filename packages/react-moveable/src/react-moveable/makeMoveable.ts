c63c1d30067e52b4115f09c6a78639177b443226
Morten Barklund <morten.barklund@gmail.com>
2022-10-13 18:59:03 +0200

    fix: Fixing bad type causing type conflict (#767)
    Without this change, the TypeScript compiler complains that `Type T does not satisfy the constraint "IObject<any>". This type parameter might need an "extends IObject<any>" constraint.`. With the above, the error goes away and the project seems to compile fine.

--- packages/react-moveable/src/react-moveable/makeMoveable.ts
+++ packages/react-moveable/src/react-moveable/makeMoveable.ts
@@ -1,7 +1,8 @@
+import { IObject } from "@daybrush/utils";
 import { Able } from "./types";
 import { InitialMoveable } from "./InitialMoveable";
 
-export function makeMoveable<T = {}>(
+export function makeMoveable<T extends IObject<any> = {}>(
     ables: Array<Able<T>>): typeof InitialMoveable & (new (...args: any[]) => InitialMoveable<T>) {
     return class Moveable extends InitialMoveable<T> {
         public static defaultAbles = ables;
