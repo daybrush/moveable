6f6e5856773bccfb681dade0a2297e59338b8787
Daybrush <younkue.choi@navercorp.com>
2023-03-13 06:25:28 +0900

    chore: fix test  runner config

--- packages/react-moveable/.storybook/test-runner.js
+++ packages/react-moveable/.storybook/test-runner.js
@@ -4,19 +4,15 @@ const { toMatchImageSnapshot } = require('jest-image-snapshot');
 const customSnapshotsDir = `${process.cwd()}/__snapshots__`;
 
 module.exports = {
-  setup() {
-    expect.extend({ toMatchImageSnapshot });
-  },
-  async postRender(page, context) {
-    if (!process.env.SKIP_TEST) {
-        return;
-    }
-    // If you want to take screenshot of multiple browsers, use
-    // page.context().browser().browserType().name() to get the browser name to prefix the file name
-    const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot({
-      customSnapshotsDir,
-      customSnapshotIdentifier: context.id,
-    });
-  },
+    setup() {
+        expect.extend({ toMatchImageSnapshot });
+    },
+    async postRender(page, context) {
+        if (!process.env.SKIP_TEST) {
+            return;
+        }
+        // If you want to take screenshot of multiple browsers, use
+        // page.context().browser().browserType().name() to get the browser name to prefix the file name
+        await page.screenshot({ path: `${customSnapshotsDir}/${context.id}.png` });
+    },
 };
