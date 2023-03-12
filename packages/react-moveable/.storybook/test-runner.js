39848f74a38826db0632f6c0f6c67f31155d7f43
Daybrush <younkue.choi@navercorp.com>
2023-03-13 06:31:37 +0900

    test: fix e2e config

--- packages/react-moveable/.storybook/test-runner.js
+++ packages/react-moveable/.storybook/test-runner.js
@@ -1,12 +1,6 @@
-// .storybook/test-runner.js
-const { toMatchImageSnapshot } = require('jest-image-snapshot');
-
 const customSnapshotsDir = `${process.cwd()}/__snapshots__`;
 
 module.exports = {
-    setup() {
-        expect.extend({ toMatchImageSnapshot });
-    },
     async postRender(page, context) {
         if (!process.env.SKIP_TEST) {
             return;
