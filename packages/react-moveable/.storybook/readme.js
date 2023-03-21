813ba18698d99b9ddb1b3827cc5950a18dda4931
Daybrush <younkue.choi@navercorp.com>
2023-03-22 03:32:17 +0900

    demo: fix readme option

--- packages/react-moveable/.storybook/readme.js
+++ packages/react-moveable/.storybook/readme.js
@@ -5,6 +5,9 @@ const readmeText = fs.readFileSync(path.resolve(__dirname, "../../../README.md")
     encoding: "utf-8",
   });
 
+  fs.mkdirSync(path.resolve(__dirname, "../stories/0-Introduction"), {
+    recursive: true,
+  });
 fs.writeFileSync(path.resolve(__dirname, "../stories/0-Introduction/readme.stories.mdx"), `
 import { Meta } from "@storybook/addon-docs";
 
