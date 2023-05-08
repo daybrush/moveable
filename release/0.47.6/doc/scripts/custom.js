3fc2960da5d2d622e0b3ec5063d0a976dd39b41e
Daybrush <younkue.choi@navercorp.com>
2023-05-08 23:36:37 +0900

    Updates

---
+++ release/0.47.6/doc/scripts/custom.js
@@ -0,0 +1,12 @@
+(function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-TRBNXHQ0ZF');
+    var script = document.createElement("script");
+
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-TRBNXHQ0Z";
+
+    document.body.appendChild(script);
+})();
