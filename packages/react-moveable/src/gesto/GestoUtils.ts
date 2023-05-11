ef37c3b6775fdfc8baa365bc793fe264c5a79764
Daybrush <younkue.choi@navercorp.com>
2023-05-12 00:20:40 +0900

    fix: add defense logic for added element on dragStart #917

--- packages/react-moveable/src/gesto/GestoUtils.ts
+++ packages/react-moveable/src/gesto/GestoUtils.ts
@@ -443,7 +443,7 @@ export function getNextTransforms(e: any) {
     return originalDatas.nextTransforms;
 }
 export function getNextTransformText(e: any) {
-    return getNextTransforms(e).join(" ");
+    return (getNextTransforms(e) || []).join(" ");
 }
 
 export function getNextStyle(e: any) {
